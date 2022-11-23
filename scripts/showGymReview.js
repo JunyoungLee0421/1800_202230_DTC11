2c61bbcb1cf820069f8589337a961d5f50ed015d
Junyoung Lee <tim000421@gmail.com>
2022-11-23 13:43:04 -0800

    Populated Reviews from database

---
+++ scripts/showGymReview.js
@@ -0,0 +1,27 @@
+let gymID = localStorage.getItem("gymID");
+
+
+function populateReviews() {
+    let gymReviewTemplate = document.getElementById("CardTemplate");
+    let gymCardGroup = document.getElementById("CardGroup");
+
+    db.collection("Reviews").where("code", "==", gymID).get()
+        .then(allReviews => {
+            reviews = allReviews.docs
+            reviews.forEach(doc => {
+                console.log(doc.data())
+                var title = doc.data().title;
+                var recommend = doc.data().recommend; 
+                var experience = doc.data().experience;
+                var description = doc.data().description;
+
+                let reviewCard = gymReviewTemplate.content.cloneNode(true);
+                reviewCard.querySelector('.title').innerHTML = title;
+                reviewCard.querySelector('.recommend').innerHTML = `recommend: ${recommend}`;
+                reviewCard.querySelector('.experience').innerHTML = `experience: ${experience}`;
+                reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
+                gymCardGroup.appendChild(reviewCard);
+            });
+        })
+}
+populateReviews();
