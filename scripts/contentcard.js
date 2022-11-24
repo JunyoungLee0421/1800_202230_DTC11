1256f8705e37311980f0770c2b401172d0301f88
aye-ron98 <aron.zhang013@gmail.com>
2022-11-23 20:03:04 -0800

    update database

--- scripts/contentcard.js
+++ scripts/contentcard.js
@@ -3,21 +3,24 @@ let ID = localStorage.getItem("gymID");
 
 function populate() {
 
-    db.collection("gyms").where("code", "==", ID).get()
+    db.collection("gym_data").where("gymID", "==", ID).get()
         .then(snap => {
             //var i = 1;  //if you want to use commented out section
             snap.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;        // get value of the "name" key
+                var title = doc.data().gym_name;        // get value of the "name" key
                 var details = doc.data().description;   // get value of the "description" key
-                var gymPic = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var gymPic = doc.data().gym_image;    //get unique ID to each hike to be used for fetching right image
                 var distance_away = doc.data().distance;    //get value of distance
                 var rate = doc.data().rating;    //get rating for each gym
                 var price_point = doc.data().price;    //get price for each gym
+                var operating_hours = doc.data().operating_hours;
+                var address = doc.data().address;
+                var city = doc.data().city;
 
                 //update title and text and image
                 document.querySelector('#gym_name').innerHTML = title;
                 document.querySelector('#details').innerHTML = details;
-                document.querySelector('#image').src = `./images/${gymPic}.jpg`; //Example: NV01.jpg
+                document.querySelector('#image').src = gymPic; //Example: NV01.jpg
                 document.querySelector('#rating').src = `./text/stars/${rate}.jpg`;
                 document.querySelector('#distance').innerHTML = distance_away;
                 document.querySelector('#price').innerHTML = price_point;
--- scripts/displaycards.js
+++ scripts/displaycards.js
@@ -7,28 +7,29 @@ function displayCards(collection) {
     db.collection(collection).get()
         .then(allGyms => {
             allGyms.forEach(doc => {
-                var title = doc.data().name;
+                var title = doc.data().gym_name;
                 var details = doc.data().description;
-                var gymID = doc.data().code;
+                var image = doc.data().gym_image;
                 var distance_away = doc.data().distance;
                 var rate = doc.data().rating;
                 var price_point = doc.data().price;
+                var id = doc.data().gymID
                 let testgymCard = GymCardTemplate.content.cloneNode(true);
 
                 testgymCard.querySelector('.card-title').innerHTML = title;
                 testgymCard.querySelector('.card-text').innerHTML = details;
-                testgymCard.querySelector('.card-image').src = `./images/${gymID}.jpg`; //Example: NV01.jpg
+                testgymCard.querySelector('.card-image').src = image; 
                 testgymCard.querySelector('.stars').src = `./text/stars/${rate}.jpg`;
                 testgymCard.querySelector('.distance').innerHTML = distance_away;
                 testgymCard.querySelector('.price').innerHTML = price_point;
-                testgymCard.querySelector('#details_btn').onclick = () => setGymData(gymID);
+                testgymCard.querySelector('#details_btn').onclick = () => setGymData(id);
                 container.appendChild(testgymCard);
             })
 
         })
 }
 
-displayCards("gyms");
+displayCards("gym_data");
 
 
 function setGymData(id) {
@@ -55,3 +56,38 @@ function insertName() {
     });
 }
 insertName();
+
+// async function getCSVdata() {
+//     // console.log("hello")
+//     const response = await fetch('/data.csv'); //send get request must be in same folder as html it is being called from
+//     const data = await response.text();      //get file response
+//     const list = data.split('\n').slice(1);  //get line
+//     list.forEach(row => {
+//         // [gymID, gym name, description, address, city, image, operating hours, rating]
+//         const columns = row.split(',');
+//         const gymID = columns[0];
+//         const gym_name = columns[1];
+//         const description = columns[2];
+//         const address = columns[3];
+//         const city = columns[4];
+//         const gym_image = columns[5];
+//         const operating_hours = columns[6];
+//         const rating = columns[7]
+//         const distance = columns[8]
+//         const price = columns[9]
+
+
+//         db.collection("gym_data").add({   //write to firestore
+//             gymID: gymID,
+//             gym_name: gym_name,
+//             description: description,
+//             address: address,
+//             city: city,
+//             gym_image: gym_image,
+//             operating_hours: operating_hours,
+//             rating: rating,
+//             distance: distance,
+//             price: price,
+    
+//         })
+//     })}
