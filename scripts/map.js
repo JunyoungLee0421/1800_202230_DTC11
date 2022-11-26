12e8e61b6cb330211590711f17a5c0e612a03fc6
aye-ron98 <aron.zhang013@gmail.com>
2022-11-25 16:48:50 -0800

    map displays gyms in user city

---
+++ scripts/map.js
@@ -0,0 +1,23 @@
+function findgyms() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userCity = userDoc.data().country;
+                    
+                    document.querySelector('#gmap_canvas').src = `https://maps.google.com/maps?q=gyms%20in%20${userCity}&t=&z=13&ie=UTF8&iwloc=&output=embed`; 
+
+                })
+        } else {
+            document.querySelector('#gmap_canvas').src = `https://maps.google.com/maps?q=gyms%20in%20burnaby&t=&z=13&ie=UTF8&iwloc=&output=embed`; 
+
+        }
+    });
+}
+findgyms();
